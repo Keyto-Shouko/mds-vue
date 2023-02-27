467162e10f20bac981f6acf4bc0f745d70e098c1
Keyto-Shouko <balvesca@gmail.com>
2023-02-27 08:57:39 +0100

    Yeah i forgot to push regularly

---
+++ Bruno_MDS_Vue/src/components/FirstDivs.vue
@@ -0,0 +1,207 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<script lang="ts">
+import mapboxgl from "mapbox-gl";
+export default {
+  data() {
+    return {
+      accessToken:
+        "pk.eyJ1IjoiYnJ1bm9hY21kcyIsImEiOiJjbGU3NmI4bnAwMmczM3duYnhjdzFiYzg3In0.U1QV92f0JnBZ1oJQvgJXDQ",
+      latLong: "",
+      age: 30,
+      firstname: "Jean",
+      year: 2022,
+      map:"" as any,
+      remember: false,
+      buttonVisible: false,
+      family: [
+        {
+          firstname: "Jojo",
+          lastname: "Bernard",
+          age: "25",
+          phoneNumber: "0606060606",
+          address: "Rue",
+          sexe: "Male",
+        },
+        {
+          firstname: "Marie",
+          lastname: "Blachère",
+          age: "29",
+          phoneNumber: "0606060606",
+          address: "Rue",
+          sexe: "Female",
+        },
+        {
+          firstname: "Jean",
+          lastname: "Bernard",
+          age: "20",
+          phoneNumber: "0606060606",
+          address: "Rue",
+          sexe: "Male",
+        },
+        {
+          firstname: "Paul",
+          lastname: "Pogba",
+          age: "17",
+          phoneNumber: "0606060606",
+          address: "Rue",
+          sexe: "Star",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    changeName(remember: boolean) {
+      this.remember = !this.remember;
+      if (remember) return (this.firstname = "Jean");
+      else {
+        return (this.firstname = "Patrick");
+      }
+    },
+    increaseAgeAndyear() {
+      this.age++;
+      this.year++;
+    },
+    addFamilyMember(submitEvent: any) {
+      this.family.push({
+        firstname: submitEvent.target.elements.firstname.value,
+        lastname: submitEvent.target.elements.lastname.value,
+        age: submitEvent.target.elements.age.value,
+        address: submitEvent.target.elements.addresse.value,
+        phoneNumber: submitEvent.target.elements.phoneNumber.value,
+        sexe: submitEvent.target.elements.sexe.value,
+      });
+      this.latLong =
+        "https://api.mapbox.com/geocoding/v5/{endpoint}/{" +
+        submitEvent.target.elements.addresse.value +
+        "}.json";
+    },
+    deleteUser(indexOfFamilyMember: number) {
+      this.family.splice(indexOfFamilyMember, 1);
+    },
+    toggleVisibleButton() {
+      this.buttonVisible = !this.buttonVisible;
+    },
+  },
+  mounted() {
+    setInterval(this.increaseAgeAndyear, 3000);
+    this.orderedUsers, (mapboxgl.accessToken = this.accessToken);
+    mapboxgl.accessToken = this.accessToken
+    this.map = new mapboxgl.Map({
+      container: "mapContainer",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [6.0934836, 45.8899344],
+      zoom: 12,
+    });
+    let marker = new mapboxgl.Marker()
+      .setLngLat([6.0934836, 45.8899344]) // Set the marker's longitude and latitude
+      .addTo(this.map); // Add the marker to the map
+
+        /*this.map.on('drag',  () => {
+            let centro = this.map.getCenter();
+            marker.setLngLat(centro);
+        });
+        this.map.on('zoom',  () => {
+            let centro = this.map.getCenter();
+            marker.setLngLat(centro);
+        });*/
+  },
+  computed: {
+    orderedUsers: function () {
+      return this.family.sort(function (a, b) {
+        let ageA = parseInt(a.age);
+        let ageB = parseInt(b.age);
+        return ageA - ageB;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="presentation ">
+    <p>Bonjour je m'appelle {{ firstname }}</p>
+  </div>
+  <br />
+  <div id="age">
+    <p>En {{ year }}, Jean aura {{ age }} ans</p>
+  </div>
+  <br />
+  <div id="family" v-for="(member, index) in family" :key="member.firstname">
+    <p>
+      {{ member.firstname }}, {{ member.lastname }}, {{ member.age }},
+      {{ member.address }}, {{ member.phoneNumber }}, {{ member.sexe }}
+    </p>
+    <button @click="deleteUser(index)">Supprime !</button>
+  </div>
+  <button v-if="buttonVisible" id="crazy-button" @click="changeName(remember)">
+    Change le nom !
+  </button>
+  <button @click="toggleVisibleButton" id="toggleCrazy">
+    Change la visibilité du crazy-button
+  </button>
+
+  <form @submit.prevent="addFamilyMember">
+    <label for="firstname">Prénom:</label><br />
+    <input
+      type="text"
+      id="firstname"
+      name="firstname"
+      placeholder="John"
+    /><br />
+    <label for="lastname">Nom:</label><br />
+    <input type="text" id="lastname" name="lastname" placeholder="Doe" /><br />
+    <label for="age">Age:</label><br />
+    <input type="text" id="age" name="age" placeholder="28" min="10" /><br />
+    <label for="sexe">Sexe:</label><br />
+    <input type="text" id="sexe" name="sexe" placeholder="Male" /><br />
+    <mapbox-address-autofill
+      access-token="pk.eyJ1IjoiYnJ1bm9hY21kcyIsImEiOiJjbGU3NmI4bnAwMmczM3duYnhjdzFiYzg3In0.U1QV92f0JnBZ1oJQvgJXDQ"
+    >
+      <label for="addresse">Addresse:</label><br />
+      <input
+        type="text"
+        id="addresse"
+        name="addresse"
+        placeholder="25 Rue"
+        autocomplete="shipping street-address"
+      /><br />
+    </mapbox-address-autofill>
+    <label for="phoneNumber">Numéro:</label><br />
+    <input
+      type="tel"
+      id="phoneNumber"
+      name="phoneNumber"
+      placeholder="0606060606"
+    /><br />
+    <button id="add-family-member" type="submit">Ajoute un membre</button>
+  </form>
+  <div id="mapContainer" class="basemap"></div>
+</template>
+
+<style>
+#app {
+  background-color: black;
+  color: white;
+  max-width: 4000px;
+  max-height: 2000px;
+}
+
+#toggleCrazy {
+  width: 100px;
+  height: 100px;
+}
+#crazy-button {
+  width: 100px;
+  height: 100px;
+}
+#mapContainer {
+  width: 600px;
+  height: 600px;
+}
+</style>
